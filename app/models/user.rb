3269f19f3875b34723ce51908e36cfddd9fcdda5
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-08 22:26:58 -0300

    :recycle: refactor: organização da criação de tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,31 @@ class User < ApplicationRecord
 
   def generate_token
 
-    if (not self.jwt_token.nil?) and (not self.jwt_token.decode_token[:error])
+    if not self.jwt_token.nil? and self.jwt_token&.decode_token[:error].nil?
       return self.jwt_token.token
     end
 
-    # Token JWT básico, para aprendizado.
+    if not self.jwt_token.nil?
+      self.jwt_token.destroy()
+    end
+
+    token = create_new_token
+    JwtToken.create!(user: self, token: token)
+    token
+  end
+
+  private
+
+  def create_new_token
+
     header = { alg: 'HS256', typ: 'JWT' }
-    payload = { user_id: self.id, exp: (Time.now + 10.minutes).to_i }
+    payload = { user_id: id, exp: (Time.now + 10.hours).to_i }
     secret = Rails.application.secret_key_base
 
     encoded_header = Base64.urlsafe_encode64(header.to_json)
     encoded_payload = Base64.urlsafe_encode64(payload.to_json)
     signature = Digest::SHA256.hexdigest("#{encoded_header}.#{encoded_payload}.#{secret}")
 
-    token = "#{encoded_header}.#{encoded_payload}.#{signature}"
-    JwtToken.create(user: self, token: token)
-    token
+    "#{encoded_header}.#{encoded_payload}.#{signature}"
   end
 end
