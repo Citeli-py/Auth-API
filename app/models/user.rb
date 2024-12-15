72b072221e03d5a45514a414b2bb241feb604ded
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-15 20:23:03 -0300

    :recycle: refactor: Adaptação para uso dos metodos expired? e JwtToken.encode

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   def generate_token
 
-    if not self.jwt_token.nil? and self.jwt_token&.decode_token[:error].nil?
+    if not (self.jwt_token.nil? or self.jwt_token.expired?)
       return self.jwt_token.token
     end
 
@@ -17,23 +17,9 @@ class User < ApplicationRecord
       self.jwt_token.destroy()
     end
 
-    token = create_new_token
-    JwtToken.create!(user: self, token: token)
+    token = JwtToken.encode({user_id: self.id})
+    self.jwt_token = JwtToken.create!(user: self, token: token)
     token
   end
 
-  private
-
-  def create_new_token
-
-    header = { alg: 'HS256', typ: 'JWT' }
-    payload = { user_id: id, exp: (Time.now + 10.hours).to_i }
-    secret = Rails.application.secret_key_base
-
-    encoded_header = Base64.urlsafe_encode64(header.to_json)
-    encoded_payload = Base64.urlsafe_encode64(payload.to_json)
-    signature = Digest::SHA256.hexdigest("#{encoded_header}.#{encoded_payload}.#{secret}")
-
-    "#{encoded_header}.#{encoded_payload}.#{signature}"
-  end
 end
