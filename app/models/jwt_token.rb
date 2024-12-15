dc2dc43d66c3837fd3932ae07ee6c513ff41865b
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-15 20:40:44 -0300

    :sparkles: feat: Uso da gem JWT e criação dos metodos encode, decode e expired?

--- app/models/jwt_token.rb
+++ app/models/jwt_token.rb
@@ -1,33 +1,39 @@
 require "json"
 require "base64"
+require "jwt"
 
 class JwtToken < ApplicationRecord
   belongs_to :user
   validates :token, presence: true
 
-  def decode_token
-    # Divida o token em suas três partes: header, payload e signature
-    header, payload, signature = self.token.split('.')
+  SECRET = Rails.application.secret_key_base
+  EXP = 10.minutes.from_now
 
-    # Decodificar o header e o payload (em Base64)
-    decoded_payload = JSON.parse(Base64.urlsafe_decode64(payload))
-
-    # Verificar se o token expirou
-    if decoded_payload['exp'].to_i < Time.now.to_i
-      raise 'Token has expired'
-    end
-
-    # Verificar se a assinatura é válida
-    secret = Rails.application.secret_key_base
-    expected_signature = Digest::SHA256.hexdigest("#{header}.#{payload}.#{secret}")
-    if expected_signature != signature
-      raise 'Invalid token signature'
-    end
-
-    # Retornar o payload se o token for válido
-    decoded_payload
-  rescue => e
-    { error: e.message }
+  def self.encode(payload, exp=EXP)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET)
   end
 
+  # Decodifica um token JWT e valida a assinatura
+  def self.decode(token)
+    begin
+      decoded = JWT.decode(token, SECRET, true, { algorithm: 'HS256' })[0]
+      HashWithIndifferentAccess.new(decoded)
+    rescue JWT::ExpiredSignature
+      raise 'Token has expired'
+    rescue JWT::VerificationError
+      raise 'Invalid token signature'
+    rescue JWT::DecodeError => e
+      raise "Invalid token: #{e.message}"
+    end
+  end
+
+  # Verifica se o token expirou
+  def expired?
+    payload = JwtToken.decode(self.token)
+    payload[:exp] < Time.now.to_i
+  rescue => e
+    # Retorna verdadeiro caso ocorra qualquer erro ao decodificar
+    true
+  end
 end
